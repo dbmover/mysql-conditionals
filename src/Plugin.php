a2496a0ff5b9e2bc0e86d26b2b600d48c09f1224
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-15 17:40:03 +0200

    add the DBMOVER_DATABASE variable

--- src/Plugin.php
+++ src/Plugin.php
@@ -16,11 +16,13 @@ class Plugin extends Conditionals\Plugin
 {
     protected function wrap(string $sql) : string
     {
+        $database = $this->loader->getDatabase();
         $tmp = 'tmp_'.md5(microtime(true));
         return <<<EOT
 DROP PROCEDURE IF EXISTS $tmp;
 CREATE PROCEDURE $tmp()
 BEGIN
+    SET DBMOVER_DATABASE = '$database';
     $sql
 END;
 CALL $tmp();
